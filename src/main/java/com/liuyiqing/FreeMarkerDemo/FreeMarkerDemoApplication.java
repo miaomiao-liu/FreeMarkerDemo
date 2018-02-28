23ca416dd19076abb44c172394a11590e2950f91
miaomiao <1242019260@qq.com>
2018-02-28 20:56:05 +0800

    firstFreeMarker

---
+++ src/main/java/com/liuyiqing/FreeMarkerDemo/FreeMarkerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.liuyiqing.FreeMarkerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeMarkerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeMarkerDemoApplication.class, args);
+	}
+}
